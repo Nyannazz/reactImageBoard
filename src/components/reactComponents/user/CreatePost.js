24539483a052e47e3e40cc04d3c01816cc708553
Nyannazz <kantemir.imam@gmail.com>
2019-05-28 00:00:27 +0200

    init branche

--- src/components/reactComponents/user/CreatePost.js
+++ src/components/reactComponents/user/CreatePost.js
@@ -14,7 +14,8 @@ export default class CreatePost extends Component {
          file: "",
          title: "",
          body: "",
-         tags: ""
+         tags: "",
+         postCreated: ""
       }
     }
   
@@ -30,7 +31,7 @@ export default class CreatePost extends Component {
     const val=file[0];
     let blob=new Blob([val])
     let objectURL=URL.createObjectURL(blob);
-    this.setState({upload:objectURL,file:val})
+    this.setState({upload:objectURL,file:val,postCreated: ''})
     console.dir(val)
     
   }
@@ -50,8 +51,8 @@ export default class CreatePost extends Component {
 
                 }
             }
-        ).then(function (response) {
-            console.dir(response.data);
+        ).then(response=> {
+            this.setState({postCreated:"POST SUCCESFULLY CREATED"})
         })
         .catch(function (error) {
             console.log(error);
@@ -70,11 +71,12 @@ export default class CreatePost extends Component {
       <div className={'uploadComp'}>
       <form onSubmit={this.sendFiles} className='uploadForm'>
         <FileInputField onFileSelect={this.changeFile}/>
-        {this.state.upload&&[
+        {(this.state.upload&&!this.state.postCreated)&&[
         <input name='title' value={this.state.title} onChange={this.onChange} placeholder='give your upload a name' key={0} type='text'></input>,
         <input name='tags' value={this.state.tags} onChange={this.onChange} placeholder='add some tags seperate by ";"' key={1} type='text'></input>,
         <textarea name='body' value={this.state.body} onChange={this.onChange} placeholder='add a comment if you want' key={2} ></textarea>,
         <input className='submitButtonMain' key={3} type='submit'></input>]}
+        {this.state.postCreated&&<div className={'postSucces centerAll'}>{this.state.postCreated}</div>}
       </form>
       <div className={'imageContainer centerAll'}>
         <img src={this.state.upload}></img>
